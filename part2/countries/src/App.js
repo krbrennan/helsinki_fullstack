bd68ae3fdccb7fb2300ceb5dac613cc89fd3436d
krbrennan <krb.brennan@gmail.com>
2021-03-30 16:02:52 -0400

    part2 countries step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,9 +9,51 @@ const Form = (props) => {
   )
 }
 
+
+const Weather = (props) => {
+console.log(props.weather)
+  if(props.weather) {
+    return(
+      <div>
+        <h2>Current Weather</h2>
+        <h4>Temperature:  {props.weather.current.temperature} Celcius</h4>
+        <img src={props.weather.current.weather_icons[0]}></img>
+        <h4>Wind: {props.weather.current.wind_speed} Km/H moving {props.weather.current.wind_dir} </h4>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <h2>Current Weather</h2>
+        <span>...Loading...</span>
+      </div>
+    )
+  }
+}
+
+
+const CountryInfo = (props) => {
+  // console.log(props)
+  return(
+    <div>
+      <h2>{props.info.countries[0].name}</h2>
+      <p>Capital: {props.info.countries[0].capital}</p>
+      <p>Population:  {props.info.countries[0].population}</p>
+      <h4>Languages:</h4>
+      <ul>
+        { props.info.countries[0].languages.map((language) => {
+            return <li>{language.name}</li>
+        })}
+      </ul>
+      <img style={{maxWidth: 250, maxHeight:250}} src={ props.info.countries[0].flag }></img>
+      <Weather weather={props.info.weather} />
+    </div>
+  )
+}
+
 const Country = (props) => {
   // console.log(props.info.name)
-  console.log(props)
+  // console.log(props)
   return(
     <form onSubmit={props.showClicked}>
       <p>{props.info.name}</p>
@@ -21,7 +63,7 @@ const Country = (props) => {
 }
 
 const FinalCountries = (props) => {
-  console.log(props)
+  // console.log(props)
   if(props.countries.length === 0) {
     return(
       <div>
@@ -30,20 +72,9 @@ const FinalCountries = (props) => {
       </div>
     )
   } else if(props.countries.length === 1) {
-      return(
-        <div>
-          <h2>{props.countries[0].name}</h2>
-          <p>Capital: {props.countries[0].capital}</p>
-          <p>Population:  {props.countries[0].population}</p>
-          <h4>Languages:</h4>
-          <ul>
-            { props.countries[0].languages.map((language) => {
-                return <li>{language.name}</li>
-            })}
-          </ul>
-          <img style={{maxWidth: 250, maxHeight:250}} src={ props.countries[0].flag }></img>
-        </div>
-      )
+    return(
+      <CountryInfo info={props} />
+    )
   } else if(props.countries.length > 10) {
       return(
         <h4>Too many matches, specify another filter</h4>
@@ -66,6 +97,9 @@ function App() {
   const [ countries, setCountries ] = useState([])
   const [ filteredCountries, setFilteredCountries ] = useState([])
 
+  const [ countryToGetWeatherFor, setCountryToGetWeatherFor ] = useState()
+  const [ weather, setWeather ] = useState()
+
 
   // get countries
   useEffect(() => {
@@ -77,10 +111,10 @@ function App() {
   }, [])
 
 
-  const clearSetSearched = () => {
-    return console.log('cleared')
-    // setSearched('')
-  }
+  // const clearSetSearched = () => {
+  //   return console.log('cleared')
+  //   // setSearched('')
+  // }
 
 
   const submitForm = (event) => {
@@ -93,8 +127,10 @@ function App() {
       // look at each country, if country.name.includes(lettersToSearch), return country
       return country.name.toLowerCase().includes(lettersToSearch)
     })
+    // console.log(`final search: ${finalSearch[0].name}`)
 
     setFilteredCountries(finalSearch)
+    getWeather(finalSearch[0].name)
     // setLettersToSearch('')
     setSearched('')
   }
@@ -106,10 +142,28 @@ function App() {
     setLettersToSearch(event.target.value.toLowerCase())
   }
 
+
+
+
+  const getWeather = (props) => {
+    console.log(props)
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=e2d7f68a9ec3a48b81bad1fabe1d3ed2&query=${props}`)
+      .then((res) => {
+        setWeather(res.data)
+      })
+  }
+
+
+
+
   const showClicked = (event) => {
     event.preventDefault()
     const countryToExpand = event.target.childNodes[0].innerHTML
     // console.log(countryToExpand)
+
+    // set country to get weather for
+    setCountryToGetWeatherFor(countryToExpand)
 
     // go through countries, filter out match, setCountries to match
     const clickedCountry = filteredCountries.filter((country) => {
@@ -119,15 +173,15 @@ function App() {
     })
 
     setFilteredCountries(clickedCountry)
-    console.log(clickedCountry)
-    // alert('clicked!')
+    getWeather(clickedCountry[0].name)
+    // console.log(clickedCountry)
   }
 
 
   return (
    <div>
      <Form onFormSubmit={submitForm} onFieldChange={ countryToSearchFor } />
-     <FinalCountries showClickedCountry={showClicked} clearSearched={clearSetSearched} countries={filteredCountries} />
+     <FinalCountries showClickedCountry={showClicked} getWeather={getWeather} countries={filteredCountries} weather={weather} />
    </div>
   );
 }
